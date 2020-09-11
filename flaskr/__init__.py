826770eb92288482b027012bce88ef01859f190d
maiyongke <273775054@qq.com>
2020-09-11 09:03:08 +0800

    新增auth蓝图

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -24,14 +24,13 @@ def create_app(test_config=None):
   except OSError:
     pass
 
-  # a simple page that says hello
-  @app.route('/hello')
-  def hello():
-    return 'Hello, World!'
-
 
   from . import db
   db.init_app(app)
+
+
+  from . import auth
+  app.register_blueprint(auth.bp)
 
 
   return app
